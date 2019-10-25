c0d33f69f62785590c41e32ae17e4655b1d874aa
Javier Porlono <pauli44ef@gmail.com>
2019-10-25 18:55:46 +0200

    fix mongoclient deprecation errors

--- server/app.js
+++ server/app.js
@@ -3,12 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const IndexModels = require('./models/IndexModels');
 const { models, connectDatabase } = IndexModels;
-
 const app = express()
 const port = process.env.PORT;
 
+
 app.use(cors())
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+connectDatabase().then(async () => {
+    app.listen(port, () => console.log(`database up and express app running on port ${port}`))
+})
+
 
--- server/models/IndexModels.js
+++ server/models/IndexModels.js
@@ -3,8 +3,11 @@ const List = require('./list')
 const Task = require('./task')
 const User = require('./user')
 
+mongoose.set('useCreateIndex', true)
+mongoose.set('useUnifiedTopology', true)
+
 const connectDatabase = () => {
-    return mongoose.connect(process.env.DATABASE_URL);
+    return mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }, );
 }
 const models = { List, Task, User };
 
